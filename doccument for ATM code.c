0ec3b79be70190d84991c117b62068f4fa5b18a0
yaswanth katta <44583720+yaswanth-katta@users.noreply.github.com>
2019-04-07 00:55:47 +0530

    Add files via upload

---
+++ doccument for ATM code.c
@@ -0,0 +1,71 @@
+                     .....doccument about the ATM code...
+* Rules and intimations for using ATM code. 
+
+
+NOTE: if you want run the any type of transection, it must be ask your secrete
+            pin number,its common for all type of transections
+
+
+ 1.If you want run the code, its necessary to specify option number,
+    which option code you have to run
+ 2.ATM secret pin number must have four digits, and it is in the form of ascendig order
+ 3.Bank account number is above the 11 digits
+ 4.ATM number must have 16 digits
+ 5.In your account have 100000 rupees fixed deposit
+ 
+* First of all, you enterd into the code,it will show you five options called
+    - How to use yash ATM 
+    -Banking
+    -Pin generate
+    -Pin change
+    -Transfer
+
+* How to use yash ATM
+  - if you are entered into this option it will shows you rules of the yash ATM
+
+* Banking
+  -It is divide into the four options called
+   ~With drawl
+   ~Balance enquiry
+   ~Ministatement
+   ~Credit
+
+~ Withdrawl
+   -Enter how much money do you want withdrawl
+   -if you are giving above information is correct then 
+    it will give you money from
+    your saving account or current account,and it will display how much balance
+    available in your accout
+   -Other wise it show you an error where did you do a mistake
+ 
+~ Balance enquiry
+    -it will show you, available balance in your saving or currrent account
+
+~ Ministatement
+    -it will ask only the secret pin number
+       (no code here)
+
+~ Credit
+    -Enter the amount, how much you have to credit
+    -If transection is succesfully completed then it show you message
+    -Other wise it will inform you, that transection was failed
+
+* Pin generate
+  -Enter your account number(above 11 digits)
+  -Enter your mobile number(which is inked to your bank account)
+  -Enter your new pin number
+  -If all the above information is correct then it show you the message calles
+    "succesfully generated"
+ -Otherwise its show you an error where did you do a mistake
+
+* Pin change
+  -Enter your new pin number
+  -If the number is satisfy the conditions of the yash ATM then
+    it will show you, the succes message
+  -Otherwise show an error where did you do a mistake
+
+*Transfer
+   -Enter the transefer ATM number(must have the 16 digits)
+   -Enter the amount, how much money do you want transfer
+   -if its succesfully transfered show you the success message
+   -Otherwise show you an error
